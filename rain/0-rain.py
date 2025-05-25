47a7d3a67ba55492e9c9906e7025e924c339982c
shomere <s.homere@alustudent.com>
2025-05-25 14:18:11 -0700

    0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -4,18 +4,21 @@
 
 def rain(walls):
     """
-    Calculate the total amount of rainwater retained
+    Calculate the total amount of rainwater retained.
+
     Args:
-        walls (list): list of non-negative integers representing wall heights
+        walls (list): List of non-negative integers representing wall heights.
+
     Returns:
-        int: total water retained
+        int: Total units of water retained after rainfall.
     """
     if not walls or len(walls) < 3:
         return 0
 
-    n = len(walls)
-    left, right = 0, n - 1
-    left_max = right_max = 0
+    left = 0
+    right = len(walls) - 1
+    left_max = 0
+    right_max = 0
     water = 0
 
     while left < right:
